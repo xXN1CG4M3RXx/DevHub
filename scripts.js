a6c0f679e7a9bc2122660ff7722b48e98fbc166c
xXN1CG4M3RXx <contenico261@gmail.com>
2026-02-05 14:25:30 +0100

    Refactor documentation pages: update content structure and styling for all pages; Revamp text display: update text rendering method

--- scripts.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const markdownElement = document.getElementById('markdown-content');
-    if (markdownElement) {
-        const markdownText = markdownElement.textContent.trim();
-        markdownElement.innerHTML = marked.parse(markdownText);
-        hljs.highlightAll();
-    }
-});
